48c428ceac6f40a65bea59faac120b1a5cd6e986
Ben Halverson <benhalverson33@gmail.com>
2022-11-04 23:39:51 -0700

    wip

--- src/controllers/openai.ts
+++ src/controllers/openai.ts
@@ -9,11 +9,38 @@ const config = new Configuration({
 const openai = new OpenAIApi(config);
 
 export const imageVariation = async (req: Request, res: Response) => {
-  const response = await openai.createImageVariation(
-    fs.createReadStream('../images/halfdome.jpg'),
-    2,
-    '1024x1024'
-  );
-  console.log('response', response);
-  res.json(response);
+  try {
+    const img = fs.createReadStream('../images/halfdome.jpg');
+    const response = await openai.createImageVariation({
+      img: img,
+      n: 1,
+      size: '1024x1024',
+    });
+    console.log('response', response);
+    res.json(response);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const imagePrompt = async (req: Request, res: Response) => {
+  const { text } = req.body;
+  try {
+    const response = await openai.createImage(
+      {
+        prompt: text,
+        n: 1,
+        size: '1024x1024',
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+    console.log('response', response);
+    res.json(response);
+  } catch (error) {
+    console.log('error', error);
+  }
 };
--- src/routes/appRoute.ts
+++ src/routes/appRoute.ts
@@ -2,13 +2,14 @@ import express from 'express';
 import { protect } from '../error/errorMiddleware';
 import { checkHealth } from '../controllers';
 import { searchImages, searchVideos } from '../controllers/search';
-import { imageVariation } from '../controllers/openai';
+import { imageVariation, imagePrompt} from '../controllers/openai';
 
 const appRoute = express.Router();
 
 appRoute.get('/health', checkHealth);
 appRoute.post('/image-search', protect(searchImages));
 appRoute.post('/video-search', protect(searchVideos));
-appRoute.get('/image-variation', protect(imageVariation));
+appRoute.get('/image-variation', imageVariation);
+appRoute.post('/image-prompt', imagePrompt);
 
 export default appRoute;
