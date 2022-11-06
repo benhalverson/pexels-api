30c28c552647b727b82262bf60b99d73af25ed5a
Ben Halverson <benhalverson33@gmail.com>
2022-11-06 14:39:40 -0800

    wip

--- src/controllers/openai.ts
+++ src/controllers/openai.ts
@@ -20,11 +20,11 @@ export const imageVariation = async (req: Request, res: Response) => {
 export const imagePrompt = async (req: Request, res: Response) => {
   const { text } = req.body;
   const response = await openai.createCompletion({
-    model: "text-davinci-002",
+    model: 'text-davinci-002',
     prompt: text,
     max_tokens: 6,
     temperature: 0,
   });
-  
+
   console.log('response', response.data);
 };
